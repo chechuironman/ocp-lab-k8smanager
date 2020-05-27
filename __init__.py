c4e845c4b5af543b0aaded1f7f1279d58456d504
Chechu <chechu@Chechus-MacBook-Pro-2.local>
2020-05-27 19:33:34 +0200

    add logging

--- __init__.py
+++ __init__.py
@@ -4,13 +4,13 @@ from flask import request
 from flask import app
 from server.routes.prometheus import track_requests
 import sys
-# import kubernetes
 sys.path.append ("/project/userapp/libraries")
-import engine,register
+import engine
 import json
 from flask_cors import CORS, cross_origin
 import os
 from logging.config import dictConfig
+from pino import pino
 
 dictConfig({
     'version': 1,
@@ -49,71 +49,26 @@ swagger_template = {
   ],
 }
 
+logger = pino(
+    bindings={"service": "k8smanager"}
+)
 
-
-# cors = CORS(app, resources={r"/": {"origins": "*"}})
-# app.config['CORS_HEADERS'] = 'Content-Type'
 app = Flask(__name__)
 CORS(app)
 swagger = Swagger(app, template=swagger_template)
 # The python-flask stack includes the prometheus metrics engine. You can ensure your endpoints
 # are included in these metrics by enclosing them in the @track_requests wrapper.
-@app.route('/hello')
-@track_requests
-def HelloWorld():
-    # To include an endpoint in the swagger ui and specification, we include a docstring that
-    # defines the attributes of this endpoint.
-    """A hello message
-    Example endpoint returning a hello message
-    ---
-    responses:
-      200:
-        description: A successful reply
-        examples:
-          text/plain: Hello from Appsody!
-    """
-    return 'Hello from Appsody!'
 
-@app.route('/namespaces')
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
-@track_requests
-def _namespaces():
-    response = engine.namespaces()
-    print(response)
-    return response
-# It is considered bad form to return an error for '/', so let's redirect to the apidocs
 @app.route('/create_user', methods = ['POST'])
 @cross_origin(origin='*',headers=['Content-Type','Authorization'])
 @track_requests
 def _create_user():
     data  = json.loads(request.data.decode("utf-8"))
-    app.logger.info('creating user: %s ', data['user'])
-    print(data)
-    # print(data['course']['text'])
-    result = engine.user_create(data['user'],data['course']['text'])
+    logger.info({'reqID': data['req_id'],'user':data['user'],'status': 'received'}, "Create user workspace for user: {} and course: {}".format(data['user'],data['course']['text']))
+    result = engine.user_create(data['user'],data['course']['text'],data['req_id'])
+    logger.info({'reqID': data['req_id'],'user':data['user'],'status': 'returned'}, "Create user workspace for user: {} and course: {}".format(data['user'],data['course']['text']))
     return result
-@app.route('/course_info', methods = ['POST'])
-# @cross_origin(origin='*',headers=['Content-Type','Authorization'])
-@track_requests
-def _course_info():
-    data  = json.loads(request.data.decode("utf-8"))
-    print(data)
 
-    # print(data['course']['text'])
-    # if data['selectedItem']:
-    result = engine.course_info(data['user'],data['courseName'])
-    return jsonify({"result": result}), 200
-    # else:
-    #   return(json.dumps({'loaded': 'False'}))
-# @app.route('/register_user', methods = ['POST'])
-# @cross_origin(origin='*',headers=['Content-Type','Authorization'])
-# @track_requests
-# def _regiser_user():
-#     result  = json.loads(request.data.decode("utf-8"))
-#     print(result)
-#     response = register.register_user(result)
-#     print(response)
-#     return str(response)
 @app.route('/')
 def index():
     return redirect('/apidocs')
--- libraries/helpers.py
+++ libraries/helpers.py
@@ -7,7 +7,11 @@ import random
 import time
 import os
 sys.path.append ("/project/userapp/libraries")
+from pino import pino
 
+logger = pino(
+    bindings={"service": "k8smanager"}
+)
 class helper:
     def __init__(self):
         self.connection = MongoClient(os.environ['MONGODB_HOST'],
@@ -39,8 +43,6 @@ class helper:
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(hostname = self.server,  username = "root", pkey = key )
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(command)
-        # print(ssh_stdin)
-        # print(ssh_stdout)
         if  ssh_stderr.readlines():
             print(ssh_stderr.readlines())
         print(ssh_stdout.readlines())   
@@ -55,15 +57,12 @@ class helper:
             db = self.connection["management"]
             collection = db["usercourse"]
             x = collection.find_one({"user":user,"courseName":course})
-            print(x)
             if x:
                 if((x['user'] == user) and (x['courseName'] == course)):
                     self.user = user
                     self.course = course
-                    print('resgistry found')
                     return True
                 else:
-                    print('resgistry NOT found')
                     return False
             else:
                 return False
@@ -79,7 +78,6 @@ class helper:
             collection = db["pivot"]
             x = collection.find_one({'name':'base'})
             self.pivot = x
-            print(x)
             return int(x['base'])
         except: 
             print('Error: {}. {}, line: {}'.format(sys.exc_info()[0],
@@ -89,12 +87,10 @@ class helper:
 
     def check_ssh_host(self):
         try:
-            print("SSH host check")
             db = self.connection["management"]
             collection = db["sshhost"]
             x = collection.find_one({'host':'cloud-lab'})
             self.ssh_host = x['ip']
-            print(x)
             return str(x['ip'])
         except: 
             print('Error: {}. {}, line: {}'.format(sys.exc_info()[0],
@@ -102,29 +98,28 @@ class helper:
                                          sys.exc_info()[2].tb_lineno))
             return False
 
-    def update_pivot(self):
+    def update_pivot(self,req_id):
         try:
             db = self.connection["management"]
             collection = db["pivot"]
             search = collection.find_one({'name':'base'})
-            print("pivot")
             x = int(search['base'])
             x += 1
             myquery = { "name": "base" }
             newvalues = { "$set": { "base": x } }
             collection.update_one(myquery, newvalues)
+            logger.info({'reqID': req_id,'user':user,'status': 'successsful'}, "Creating user {} and course {} Increase the pivot".format(user,course))
             return True
         except: 
+            logger.error({'reqID': req_id,'user':user,'status': 'ERROR'}, "Error Creating user {} and course {} Increase the pivot. Error: {} {} {}".format(user,course,sys.exc_info()[0],sys.exc_info()[1],sys.exc_info()[2].tb_lineno))
             print('Error: {}. {}, line: {}'.format(sys.exc_info()[0],
                                          sys.exc_info()[1],
                                          sys.exc_info()[2].tb_lineno))
             return False
 
-    def create_user_registry(self,user,course):
+    def create_user_registry(self,user,course,req_id):
         try:
-            print("Create course: {} for user {}".format(course,user))
             ssh_host = self.check_ssh_host()
-            print(ssh_host)
             db = self.connection["management"]
             collection = db["courses"]
             self.user = user
@@ -132,25 +127,27 @@ class helper:
             document = collection.find_one({'courseName':course})
             collection = db["usercourse"]
             x = collection.insert_one({'user':user,'courseName':course,'github':document['github'], 'oc_instance': document['clusterUrl'], 'ssh_host' : self.ssh_host})
+            logger.info({'reqID': req_id,'user':user,'status': 'successsful'}, "Creating user {} and course {} MongoDB registry".format(user,course))
             return True
-        except: 
+        except:
+            logger.error({'reqID': req_id,'user':user,'status': 'ERROR'}, "Error Creating user {} and course {} MongoDB registry. Error: {} {} {}".format(user,course,sys.exc_info()[0],sys.exc_info()[1],sys.exc_info()[2].tb_lineno))
             print('Error: {}. {}, line: {}'.format(sys.exc_info()[0],
                                          sys.exc_info()[1],
                                          sys.exc_info()[2].tb_lineno))
             return False
 
-    def workspace(self,user,course):
+    def workspace(self,user,course,req_id):
         try:
-            print("Workspace: {} for user {}".format(course,user))
             db = self.connection["management"]
             collection = db["usercourse"]
             document = collection.find_one({'user':user,'courseName':course})
             response = []
             document['_id'] = str(document['_id'])
-            print("chechu {}".format(document))
             response.append(document)
+            logger.info({'reqID': req_id,'user':user,'status': 'successsful'}, "READING user {} and course {} Workspace".format(user,course))
             return response
         except: 
+            logger.error({'reqID': req_id,'user':user,'status': 'ERROR'}, "Error READING user {} and course {}  Workspace. Error: {} {} {}".format(user,course,sys.exc_info()[0],sys.exc_info()[1],sys.exc_info()[2].tb_lineno))
             print('Error: {}. {}, line: {}'.format(sys.exc_info()[0],
                                          sys.exc_info()[1],
                                          sys.exc_info()[2].tb_lineno))
@@ -158,24 +155,20 @@ class helper:
 
     def update_user_registry(self,user,course,parameters):
         try:
-            print("Update course: {} for user {}".format(course,user))
             db = self.connection["management"]
             collection = db["usercourse"]
             myquery = { "user": user,"courseName":course }
-            # print(myquery)
             newvalues = { "$set":  parameters  }
-            # print('hola')
-            # print(newvalues)
             result = collection.update_one(myquery, newvalues)
         except: 
+            logger.error({'reqID': req_id,'user':user,'status': 'progressing'}, "Error creating MongoDB registry user: {} and course: {}. Error: {} {} {}".format(user,course,sys.exc_info()[0],sys.exc_info()[1],sys.exc_info()[2].tb_lineno))
             print('Error: {}. {}, line: {}'.format(sys.exc_info()[0],
                                          sys.exc_info()[1],
                                          sys.exc_info()[2].tb_lineno))
             return False
 
-    def create_user_oc(self):
+    def create_user_oc(self,req_id):
         try:
-            print("Create oc user: {} ".format(self.user))
             passwd =  self.randomString()
             self.oc_passwd = passwd
             self.oc_user = self.ssh_user
@@ -183,66 +176,54 @@ class helper:
                 command = 'htpasswd -b /mnt/lab/users.htpasswd {} {}'.format(self.oc_user,self.oc_passwd)
             else:
                 command = 'htpasswd -c -B -b /mnt/lab/users.htpasswd {} {}'.format(self.oc_user,self.oc_passwd)
-
-            print(command)
             os.system(command)
-            # create_oc_user = self.ssh(command)
             command = 'oc --kubeconfig=/project/userapp/libraries/kubeconfig/kubeconfig delete secret htpass-secret -n openshift-config'
-            print(command)
             os.system(command)
-            # create_oc_user = self.ssh(command)
             command = 'oc --kubeconfig=/project/userapp/libraries/kubeconfig/kubeconfig create secret generic htpass-secret --from-file=htpasswd=/mnt/lab/users.htpasswd -n openshift-config'
-            print(command)
             os.system(command)
-            # create_oc_user = self.ssh(command)
             parameters = {"oc_user":self.oc_user,"oc_passwd":self.oc_passwd}
-            print(parameters)
-            # print(parameters)
             update_registry = self.update_user_registry(self.user,self.course,parameters)
+            logger.info({'reqID': req_id,'user':user,'status': 'successsful'}, "Creating user {} and course {} OpenShift User".format(user,course))
         except: 
+            logger.error({'reqID': req_id,'user':user,'status': 'ERROR'}, "Creating user {} and course {} OpenShift User. Error: {} {} {}".format(user,course,sys.exc_info()[0],sys.exc_info()[1],sys.exc_info()[2].tb_lineno))
             print('Error: {}. {}, line: {}'.format(sys.exc_info()[0],
                                          sys.exc_info()[1],
                                          sys.exc_info()[2].tb_lineno))
             return False
 
-    def create_project_oc(self):
+    def create_project_oc(self,req_id):
         try:
             self.oc_project = self.ssh_user
             command = 'oc --kubeconfig=/project/userapp/libraries/kubeconfig/kubeconfig new-project {}'.format(self.oc_project)
-            # create_oc_user = self.ssh(command)
             os.system(command)
             command = 'oc --kubeconfig=/project/userapp/libraries/kubeconfig/kubeconfig adm policy add-role-to-user edit {} -n {}'.format(self.oc_user,self.oc_project)
             os.system(command)
-            # create_oc_user = self.ssh(command)
             parameters = {"oc_project":self.oc_project}
-            # print(parameters)
             update_registry = self.update_user_registry(self.user,self.course,parameters)
+            logger.info({'reqID': req_id,'user':user,'status': 'successsful'}, "Creating user {} and course {} OpenShift Project".format(user,course))
         except: 
+            logger.error({'reqID': req_id,'user':user,'status': 'ERROR'}, "Error Creating user {} and course {} OpenShift Project. Error: {} {} {}".format(user,course,sys.exc_info()[0],sys.exc_info()[1],sys.exc_info()[2].tb_lineno))
             print('Error: {}. {}, line: {}'.format(sys.exc_info()[0],
                                          sys.exc_info()[1],
                                          sys.exc_info()[2].tb_lineno))
             return False
 
-    def create_ssh_user(self,user,course):
+    def create_ssh_user(self,user,course,req_id):
         try:
-            print("Create ssh user: {} ".format(user))
-            # print("user {}".format(self.user))
             passwd =  self.randomString()
             pivot = self.check_pivot()
             ssh_user = 'user{}'.format(pivot)
             self.ssh_user = ssh_user
-            # self.ssh('ls')
             command = 'useradd    {}'.format(ssh_user)
             create_user = self.ssh(command) 
-            # print(create_user)
             command = 'echo "{}" | passwd {} --stdin '.format(passwd,ssh_user)
-            print(command)
             assign_passwd = self.ssh(command)  
             self.ssh_passwd = assign_passwd
             parameters = {"ssh_user":ssh_user,"ssh_passwd":passwd}
-            print(parameters)
-            update_registry = self.update_user_registry(user,course,parameters)    
+            update_registry = self.update_user_registry(user,course,parameters)   
+            logger.info({'reqID': req_id,'user':user,'status': 'successsful'}, "Creating user {} and course {} SSH user".format(user,course)) 
         except:
+            logger.error({'reqID': req_id,'user':user,'status': 'ERROR'}, "Error Creating user {} and course {} SSH user. Error: {} {} {}".format(user,course,sys.exc_info()[0],sys.exc_info()[1],sys.exc_info()[2].tb_lineno))
             print('Error: {}. {}, line: {}'.format(sys.exc_info()[0],
                                          sys.exc_info()[1],
                                          sys.exc_info()[2].tb_lineno))
@@ -258,6 +239,7 @@ class helper:
             result = self.workspace(user,course)
             return self.workspace(user,course)
         except:
+            logger.error({'reqID': req_id,'user':user,'status': 'progressing'}, "Error creating MongoDB registry user: {} and course: {}. Error: {} {} {}".format(user,course,sys.exc_info()[0],sys.exc_info()[1],sys.exc_info()[2].tb_lineno))
             print('Error: {}. {}, line: {}'.format(sys.exc_info()[0],
                                          sys.exc_info()[1],
                                          sys.exc_info()[2].tb_lineno))
